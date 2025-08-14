a067a3e951c041770e24f94c0f1b85b7c1867485
Devshasa <shasa.thuo@gmail.com>
2025-08-14 13:49:45 +0300

    remove next

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Apple } from "lucide-react";
 export default function Home() {
 	return (
-		<div className={"h-screen flex items-center justify-center"}>
-			<Button variant={"default"} className="rounded-full">
-        <Apple />
-				Harro
-			</Button>
+		<div className={""}>
+
 		</div>
 	);
 }

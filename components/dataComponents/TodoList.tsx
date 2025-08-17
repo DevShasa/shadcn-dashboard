8c99d1635712456b5162e6440c5ad8fa595a0456
Devshasa <shasa.thuo@gmail.com>
2025-08-17 20:49:30 +0300

    Changes

---
+++ components/dataComponents/TodoList.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "../ui/scroll-area"
+const TodoList = () => {
+  return (
+    <div>
+        Calendar
+        <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
+            
+        </ScrollArea>
+    </div>
+  )
+}
+
+export default TodoList
